74ad9618733c74b91f9b16aa30d5d1c3ad15bba9
KokhtyrevaOlga <s1063@yandex.ru>
2018-10-04 01:39:53 +0300

    Update Project.sol

--- contracts/Project.sol
+++ contracts/Project.sol
@@ -1,10 +1,35 @@
-pragma solidity ^0.4.13;
+pragma solidity ^0.4.16;
 
-contract Project {
+contract AbstractProject {
+  
+  function balanceOf(address _who) public view returns (uint256);
 
-	uint256 public balance = 0;
-	
-	function() payable public {
-		balance = balance + msg.value;
-	}  
+  //function transfer(address _to, uint256 _value) public returns (bool);
+
+  function transferFrom(address _from, address _to, uint256 _value)
+    public returns (bool);
+}
+
+contract Project is AbstractProject {
+    
+    address public owner;
+    mapping (address => uint256) public balances;
+    
+    constructor () public {
+			owner = msg.sender;
+		} 
+		
+	function() public payable {
+        balances[msg.sender] = balances[msg.sender] + msg.value;
+    }
+
+    function balanceOf(address _who) public view returns (uint256) {
+        return balances[_who];
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool) {
+        require(balances[_from] >= _value);
+        balances[_from] = balances[_from] - _value;
+        balances[_to] = balances[_to] + _value;
+    }
 }
